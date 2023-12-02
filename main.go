0275b976ff2c8bbbbd503a4c02297c6a0245816b
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-03 00:58:12 +0100

    setting things up

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName string = "Aries Launch"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+}
