3d50da545d541f94f277bab7a59582259fa80b1f
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-06 02:25:53 +0100

    fixed user ticket order logic

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ func main() {
 	var conferenceName string = "Aries Launch"
 	const conferenceTickets uint = 50
 	var remainingTickets uint = 50
-	var bookings [50]string //array
-	var book2 []string      //slice
+	//	var bookings [50]string //array
+	var book2 []string //slice
 	//bookings := []string{} //method two of declaring slice
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
@@ -38,26 +38,29 @@ func main() {
 		fmt.Println("Enter number of tickets you want: ")
 		fmt.Scan(&userTickets)
 
-		if userTickets <= remainingTickets {
-			book2 = append(book2, firstName+""+lastName) //slice
-
-			firstNames := []string{}
-			for _, booking := range book2 {
-				var names = strings.Fields(booking) //splits the string with white space as separator
-				if len(names) > 0 {
-					firstNames = append(firstNames, names[0])
-				}
-
-			}
-			fmt.Printf("The first names of ticket holders: %v\n", firstNames)
-			remainingTickets = remainingTickets - userTickets
-			fmt.Printf("Thank you for booking, %v. You will receive a confirmation email at %v. See you at the Con\n", firstName, email)
-
-		} else {
-			fmt.Println("Please pick a valid number, we do not have that many tickets")
+		if userTickets > remainingTickets {
+			fmt.Printf("sorry we do not have that many tickets")
 			break
 		}
+		remainingTickets = remainingTickets - userTickets
 
+		book2 = append(book2, firstName+""+lastName) //slice
+		fmt.Printf("Thank you for booking, %v. You will receive a confirmation email at %v. See you at the Con\n", firstName, email)
+
+		firstNames := []string{}
+		for _, booking := range book2 {
+			var names = strings.Fields(booking) //splits the string with white space as separator
+			if len(names) > 0 {
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of ticket holders: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Sorry, we are booked out")
+				break
+			}
+
+		}
 	}
 
 }
