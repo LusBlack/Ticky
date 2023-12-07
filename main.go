26f6bb26b02061b4527fbd16934650166239540c
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-07 21:18:10 +0100

    restructuring ticket logic and adding function

--- main.go
+++ main.go
@@ -13,11 +13,9 @@ func main() {
 	var book2 []string //slice
 	//bookings := []string{} //method two of declaring slice
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("we have %v tickets left \n", remainingTickets)
-	fmt.Println("Get your tickets here to attend")
-
-	for remainingTickets > 0 && len(book2) < 50 {
+	greetings(conferenceName, remainingTickets, conferenceTickets)
+	fNames()
+	for {
 
 		var (
 			firstName   string
@@ -26,6 +24,7 @@ func main() {
 			email       string
 		)
 
+		//Request User input using pointers to await/delay
 		fmt.Println("Enter your first name: ")
 		fmt.Scan(&firstName)
 
@@ -38,30 +37,52 @@ func main() {
 		fmt.Println("Enter number of tickets you want: ")
 		fmt.Scan(&userTickets)
 
-		if userTickets <= remainingTickets {
-			remainingTickets = remainingTickets - userTickets
+		//Validate input
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		//conditionals for response
+		if isValidName && isValidEmail && isValidTicketNumber {
 			book2 = append(book2, firstName+""+lastName) //slice
 
 			fmt.Printf("Thank you for booking, %v. You will receive a confirmation email at %v. See you at the Con\n", firstName, email)
+			remainingTickets = remainingTickets - userTickets
 
-			firstNames := []string{}
-			for _, booking := range book2 {
-				var names = strings.Fields(booking) //splits the string with white space as separator
-				if len(names) > 0 {
-					firstNames = append(firstNames, names[0])
-				}
-				fmt.Printf("The first names of ticket holders: %v\n", firstNames)
-
-				if remainingTickets == 0 {
-					fmt.Println("Sorry, we are booked out")
-					break
-				}
+			//logic to handle no tickets scenario
+			if remainingTickets == 0 {
+				fmt.Println("Sorry, we are booked out")
+				break
 			}
-
 		} else {
-			fmt.Println("Sorry, we do not have that many tickets")
-			continue
+			if !isValidName {
+				fmt.Println("Please fill in name field correctly(cannot be less than 3 characters)")
+			}
+			if !isValidEmail {
+				fmt.Println("Sorry your email is invalid")
+			}
+			if !isValidTicketNUmber {
+				fmt.Println("Please input a valid ticket number")
+			}
 		}
 	}
+}
 
+func greetUsers(confName string, remTickets int, confTickets int) {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("we have %v tickets left \n", remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func fNames(book2 []string) {
+	//formatting user details
+	firstNames := []string{}
+	for _, booking := range book2 {
+		var names = strings.Fields(booking) //splits the string with white space as separator
+		if len(names) > 0 {
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("The first names of ticket holders: %v\n", firstNames)
+
+	}
 }
