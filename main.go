5d089a551f8d1636dff6b72ce8fa88178f8cc3e9
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-12-08 04:35:17 +0100

    organising functions and separating concerns

--- main.go
+++ main.go
@@ -1,53 +1,36 @@
 package main
 
 import (
+	"Ticky/helper"
 	"fmt"
 	"strings"
 )
 
-func main() {
-	var conferenceName string = "Aries Launch"
-	const conferenceTickets uint = 50
-	var remainingTickets uint = 50
-	//	var bookings [50]string //array
-	var book2 []string //slice
-	//bookings := []string{} //method two of declaring slice
+const conferenceTickets uint = 50
 
-	greetings(conferenceName, remainingTickets, conferenceTickets)
-	fNames()
+var conferenceName string = "Aries Launch"
+var remainingTickets uint = 50
+var book2 []string //slice
+//bookings := []string{} //method two of declaring slice
+
+func main() {
+
+	greetings()
+
 	for {
 
-		var (
-			firstName   string
-			lastName    string
-			userTickets uint
-			email       string
-		)
+		//getting user input
+		firstName, lastName, email, userTickets := getUserInput()
 
-		//Request User input using pointers to await/delay
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
-
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Enter your email address: ")
-		fmt.Scan(&email)
-
-		fmt.Println("Enter number of tickets you want: ")
-		fmt.Scan(&userTickets)
-
-		//Validate input
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		//validate user input
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		//conditionals for response
 		if isValidName && isValidEmail && isValidTicketNumber {
-			book2 = append(book2, firstName+""+lastName) //slice
+			bookTicket()
 
-			fmt.Printf("Thank you for booking, %v. You will receive a confirmation email at %v. See you at the Con\n", firstName, email)
-			remainingTickets = remainingTickets - userTickets
+			fNames := getFNames()
+			fmt.Printf("The first names of bookings are: %v\n", fNames)
 
 			//logic to handle no tickets scenario
 			if remainingTickets == 0 {
@@ -68,13 +51,13 @@ func main() {
 	}
 }
 
-func greetUsers(confName string, remTickets int, confTickets int) {
+func greetUsers() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("we have %v tickets left \n", remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
 
-func fNames(book2 []string) {
+func getFNames() {
 	//formatting user details
 	firstNames := []string{}
 	for _, booking := range book2 {
@@ -82,7 +65,39 @@ func fNames(book2 []string) {
 		if len(names) > 0 {
 			firstNames = append(firstNames, names[0])
 		}
-		fmt.Printf("The first names of ticket holders: %v\n", firstNames)
-
 	}
+	return firstNames
+}
+
+func getUserInput() (string, string, uint, string) {
+	var (
+		firstName   string
+		lastName    string
+		userTickets uint
+		email       string
+	)
+
+	//Request User input using pointers to await/delay
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets you want: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, userTickets, email
+
+}
+
+func bookTicket(userTickets uint, book2 []string, firstName string, lastName string) {
+	remainingTickets = remainingTickets - userTickets
+	book2 = append(book2, firstName+""+lastName) //slice
+
+	fmt.Printf("Thank you for booking, %v. You will receive a confirmation email at %v. See you at the Con\n", firstName, email)
+
 }
